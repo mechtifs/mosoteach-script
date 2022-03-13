c8ac299b9cc116b5ba8a401708060ae2218343a6
Miya Rin <mechtifs@gmail.com>
2022-03-13 21:30:21 +0800

    No longer needs to get token & clazz_course_id

--- mosoteach.py
+++ mosoteach.py
@@ -2,46 +2,69 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Fill in your class course ID and login token here
-clazz = 'class_couse_id'
-token = 'login_token'
+# Fill in your account and password here
+json = {'account': '10086', 'password': '123456'}
 
-# Main function
+# Log in
 headers = {
-    'Host': 'www.mosoteach.cn',
-    'Cookie': 'login_token='+token,
+    'Host': 'coreapi-proxy.mosoteach.cn',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.7113.93 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate',
-    'Referer': 'https://www.mosoteach.cn/web/index.php?c=interaction&m=index&clazz_course_id='+clazz,
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
+    'Content-Type': 'application/json',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
     'Cache-Control': 'max-age=0',
     'Te': 'trailers',
     'Connection': 'close',
 }
-r = requests.get(
-    'https://www.mosoteach.cn/web/index.php?c=res&m=index&clazz_course_id='+clazz, headers=headers)
-soup = BeautifulSoup(r.text, 'html.parser')
-headers['Referer'] = 'https://www.mosoteach.cn/web/index.php?c=res&m=index&clazz_course_id='+clazz
-headers.update({
-    'Origin': 'https://www.mosoteach.cn',
-    'X-Requested-With': 'XMLHttpRequest',
-})
-for res in soup.select('.res-row-open-enable'):
-    if res.select('span')[9].attrs['style'] == 'color:#8fc31f':
-        continue
-    dur = str(round(float((res.select('span')[5].contents[0].split()[0]))*60))
-    data = {
-        'clazz_course_id': clazz,
-        'res_id': res.attrs['data-value'],
-        'watch_to': dur,
-        'duration': dur,
-        'current_watch_to': dur,
-    }
-    print(data)
-    r = requests.post(
-        'https://www.mosoteach.cn/web/index.php?c=res&m=save_watch_to', headers=headers, data=data)
+r = requests.post(
+    'https://coreapi-proxy.mosoteach.cn/index.php/passports/account-login', headers=headers, json=json)
+headers['Host'] = 'www.mosoteach.cn'
+try:
+    headers.update({
+        'Cookie': 'login_token='+r.json()['token'],
+        'Sec-Fetch-User': '?1',
+    })
+except KeyError:
+    quit('Wrong account or password!')
+print('Login token: '+r.json()['token'])
+
+# Get class course ID
+r = requests.post(
+    'https://www.mosoteach.cn/web/index.php?c=clazzcourse&m=my_joined', headers=headers)
+headers.pop('Content-Type')
+headers['Sec-Fetch-Dest'] = 'document'
+headers['Sec-Fetch-Mode'] = 'navigate'
+headers['Sec-Fetch-Site'] = 'same-origin'
+for clazz in r.json()['data']:
+    print('Class ID: '+clazz['id'])
+
+    # Get video ID
+    r = requests.get(
+        'https://www.mosoteach.cn/web/index.php?c=res&m=index&clazz_course_id='+clazz['id'], headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    headers['Referer'] = 'https://www.mosoteach.cn/web/index.php?c=res&m=index&clazz_course_id='+clazz['id']
+    headers.update({
+        'Origin': 'https://www.mosoteach.cn',
+        'X-Requested-With': 'XMLHttpRequest',
+    })
+    for res in soup.select('.res-row-open-enable'):
+        if res.select('span')[9].attrs['style'] == 'color:#8fc31f':
+            continue
+        dur = str(
+            round(float((res.select('span')[5].contents[0].split()[0]))*60))
+        print('Video ID: '+res.attrs['data-value']+', duration: '+dur+'s')
+        data = {
+            'clazz_course_id': clazz['id'],
+            'res_id': res.attrs['data-value'],
+            'watch_to': dur,
+            'duration': dur,
+            'current_watch_to': dur,
+        }
+
+        # Save watch time
+        r = requests.post(
+            'https://www.mosoteach.cn/web/index.php?c=res&m=save_watch_to', headers=headers, data=data)
